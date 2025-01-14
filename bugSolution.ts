6042785013c4e703ae392577d5b355fbea43015e
BugHunter-X <askzenai@gmail.com>
2025-01-14 17:10:08 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,28 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5); // This works fine
+
+function printNumbers2(n: number): void {
+  if (n <= 0) {
+    return;
+  }
+  console.log(n);
+  printNumbers2(n - 1);
+}
+
+printNumbers2(5); // This also works fine
+
+//The solution is in the following function
+function printNumbers3(n: number): void {
+  if (n <= 0) {
+    return;
+  }
+  console.log(n);
+  printNumbers3(n - 1);
+}
+
+printNumbers3(5); //This will print 5,4,3,2,1
